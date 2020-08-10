637b48a1dbebcfbe7ce5bbe3b25865c7b4cc468a
Ryan Cuvillier <cuvillier.ryan@gmail.com>
2020-08-10 15:24:16 -0500

    update

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,2 +1,38 @@
+import java.util.Scanner;
+
 public class ConsoleExercises {
+    public static void main(String[] args) {
+        double pi = 3.14159;
+        System.out.printf("\n\n%.2f \n\n" , pi);
+
+        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Enter a integer: ");
+//        int userInput = scanner.nextInt();
+//        System.out.println("You entered: --> \"" + userInput + "\" <--");
+//
+//        System.out.print("Enter 3 words: ");
+//        String userFirstInput = scanner.next();
+//        String userSecondInput = scanner.next();
+//        String userThirdInput = scanner.next();
+//        System.out.println("You entered: --> \"" + userFirstInput + "\" <--");
+//        System.out.println("You entered: --> \"" + userSecondInput + "\" <--");
+//        System.out.println("You entered: --> \"" + userThirdInput + "\" <--");
+//
+            scanner.nextLine();
+//        System.out.print("\nEnter a sentence: ");
+//        String sentence = scanner.nextLine();
+//        System.out.println("You entered: --> \"" + sentence + "\" <--");
+//
+//        System.out.print("\nEnter the room length: ");
+//        String sLength = scanner.nextLine();
+//        int length = Integer.parseInt(sLength);
+//
+//        System.out.print("\nEnter the room width: ");
+//        String sWidth = scanner.nextLine();
+//        int width = Integer.parseInt(sWidth);
+//
+//        System.out.println("The area of the room is: --> \"" + length * width + "\" <--");
+//        System.out.println("The perimeter of the room is: --> \"" + (2*length + 2*width) + "\" <--");
+
+    }
 }
--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,45 +1,45 @@
 import java.sql.SQLOutput;
-
-import java.util.Scanner;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-
-//        int myFavoriteNumber = 9;
-//        System.out.println(myFavoriteNumber);
 //
-//        String myString = "String";
-//        System.out.println(myString);
+//import java.util.Scanner;
 //
-////        char myString = 'D';
+//public class HelloWorld {
+//    public static void main(String[] args) {
+//        System.out.println("Hello, World!");
+//
+////        int myFavoriteNumber = 9;
+////        System.out.println(myFavoriteNumber);
+////
+////        String myString = "String";
 ////        System.out.println(myString);
-//
-////        long myNumber = 123;
+////
+//////        char myString = 'D';
+//////        System.out.println(myString);
+////
+//////        long myNumber = 123;
+//////        System.out.println(myNumber);
+////
+////        float myNumber = 3.14F;
 ////        System.out.println(myNumber);
+////
+////        int x = 5;
+////        System.out.println(x++);
+////        System.out.println(x);
+////
+////        int y = 5;
+////        System.out.println(++y);
+////        System.out.println(y);
 //
-//        float myNumber = 3.14F;
-//        System.out.println(myNumber);
+////        String theNumberThree = "three";
+////        Object o = theNumberThree;
+////        int three = (int) o;
+////        System.out.println(three);
+//three
+//        System.out.print("\n\nEnter something: ");
+//        String userInput = scanner.next();
+//        System.out.println("You entered: --> \"" + userInput + "\")
 //
-//        int x = 5;
-//        System.out.println(x++);
-//        System.out.println(x);
 //
-//        int y = 5;
-//        System.out.println(++y);
-//        System.out.println(y);
-
-//        String theNumberThree = "three";
-//        Object o = theNumberThree;
-//        int three = (int) o;
-//        System.out.println(three);
-
-        System.out.print("\n\nEnter something: ");
-        String userInput = scanner.next();
-        System.out.println("You entered: --> \"" + userInput + "\")
-
-
-    }
-}
-
+//    }
+//}
+//
 
