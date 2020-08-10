a98d5fa49b23878d0d6adc0938f11492e69604eb
Ryan Cuvillier <cuvillier.ryan@gmail.com>
2020-08-10 09:55:49 -0500

    update

---
+++ src/HelloWorld.java
@@ -0,0 +1,45 @@
+import java.sql.SQLOutput;
+
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+//        int myFavoriteNumber = 9;
+//        System.out.println(myFavoriteNumber);
+//
+//        String myString = "String";
+//        System.out.println(myString);
+//
+////        char myString = 'D';
+////        System.out.println(myString);
+//
+////        long myNumber = 123;
+////        System.out.println(myNumber);
+//
+//        float myNumber = 3.14F;
+//        System.out.println(myNumber);
+//
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+//
+//        int y = 5;
+//        System.out.println(++y);
+//        System.out.println(y);
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+//        System.out.println(three);
+
+        System.out.print("\n\nEnter something: ");
+        String userInput = scanner.next();
+        System.out.println("You entered: --> \"" + userInput + "\")
+
+
+    }
+}
+
+
