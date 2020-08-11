bbe0f7cb226d7b2153b332891709fd888032f696
Ryan Cuvillier <cuvillier.ryan@gmail.com>
2020-08-11 09:27:33 -0500

    update

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,28 @@
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+
+//        int i = 5;
+//        while (i <= 15) {
+//            System.out.print(i);
+//            ++i;
+//        }
+//        int num = 0;
+//        do {
+//            num += 2;
+//            System.out.println(num);
+//        } while (num < 100);
+
+//        int num = 100;
+//        do {
+////            num += 2;
+//            System.out.println(num);
+//            num -= 5;
+//        } while (num >= -10);
+
+        long i = 2;
+        do {
+            System.out.println("\t%d\n", i);
+            i *= 1;
+        } while (i <= 1_000_000);
+    }
+}
