b2f9630cb266a03c8e7f4edbb294ce4ed0b43fe4
Ryan Cuvillier <cuvillier.ryan@gmail.com>
2020-08-12 13:24:47 -0500

    update

---
+++ src/MethodsExercises.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+import java.util.*;
+import org.w3c.dom.ls.LSOutput;
+
+public class MethodsExercises {
+    public static void main(String[] args) {
+        System.out.println(add(1,6));
+        System.out.println(subtract(8,6));
+        System.out.println(multiply(7,5));
+        System.out.println(divide(10,5));
+        System.out.println(modulus(8, 3));
+        System.out.println(Multiply(5,5));
+    }
+
+    // Addition method
+    public static int add(int x, int y) {
+        return x + y;
+    }
+
+    //Subtraction method
+    public static int subtract(int x, int y) {
+        return x - y;
+    }
+
+    //Multiplication method
+    public static int multiply(int x, int y) {
+        return x * y;
+    }
+
+    //Division method
+    public static int divide(int x, int y) {
+        return x / y;
+    }
+
+    //Modulus method
+    public static int modulus(int x, int y){
+        return x % y;
+    }
+
+    public static int Multiply(int x, int y){
+        int result = 0;
+        while(x > 0){
+            result += y;
+            x--;
+        }
+        return result;
+    }
+
+
+
+
+
+
+}
