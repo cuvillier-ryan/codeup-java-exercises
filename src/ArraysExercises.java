89f2729a2b60a5589bb77b5a58a5d8d1d5cb9308
Ryan Cuvillier <cuvillier.ryan@gmail.com>
2020-08-17 09:04:40 -0500

    update

---
+++ src/ArraysExercises.java
@@ -0,0 +1,17 @@
+
+import java.util.Arrays;
+
+public class ArraysExercises {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        System.out.println(Arrays.toString(numbers));
+        // we need the .toString method to display the actual array data.
+
+        String[] persons = new String[3];
+
+
+
+    }
+
+
+}
---
+++ src/myJava.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.Random;
+
+public class myJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        double result = (double)10 / (double)3;
+        System.out.println(result);
+
+
+//Final is to set a variable to const.
+//        final float pi = 3.14F;
+//        System.out.println(pi);
+
+
+// Going to be practicing 2D and 3D arrays.
+//        int[][] numbers = {{1,2,3}, {4,5,6}};
+//        System.out.println(Arrays.deepToString(numbers));
+
+
+//        int[] numbers = {2, 3, 5, 1, 4};
+//        Arrays.sort(numbers);
+//        System.out.println(numbers.length);
+//        System.out.println(Arrays.toString(numbers));
+
+    }
+}
