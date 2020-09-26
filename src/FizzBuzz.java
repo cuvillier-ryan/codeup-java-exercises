8cfc76b3bb3e95d7b1d32650240cda201b93d294
Ryan Cuvillier <cuvillier.ryan@gmail.com>
2020-09-26 16:44:28 -0500

    update

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -24,22 +24,28 @@ public class FizzBuzz {
                 System.out.println(k);
             }
         }
+
+
+        //For some reason this fizzBuzz2 called method is being ignored.
+        fizzBuzz2(80);
+
     }
 
-//    public static String fizzBuzz(int number){
+    public static String fizzBuzz2(int number){
+        if (number % 15 == 0){
+            return "fizzBuzz";
+        }else if (number % 5 == 0){
+            return "buzz";
+        }else if (number % 3 == 0){
+            return "fizz";
+        }
+        return String.valueOf(number);
+    }
+
+
+    //    public static String fizzBuzz(int number){
 //        if(b)
 //    }
-
-
-
-
-
-
-
-
-
-
-
 
 
 }
