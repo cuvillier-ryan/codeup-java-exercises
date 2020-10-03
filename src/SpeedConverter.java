e9b45812b0f4d4afe230234c84bc5522dfcdc140
Ryan Cuvillier <cuvillier.ryan@gmail.com>
2020-10-03 11:42:13 -0500

    java practice

---
+++ src/SpeedConverter.java
@@ -0,0 +1,20 @@
+public class SpeedConverter {
+    //write code here
+    public static long toMilesPerHour(double kilometerPerHour) {
+        if (kilometerPerHour < 0) {
+            return -1;
+        }
+        return Math.round(kilometerPerHour / 1.609);
+
+    }
+
+    public static void printConversion(double kilometersPerHour) {
+
+        if (kilometersPerHour < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            long milesPerHour = toMilesPerHour(kilometersPerHour);
+            System.out.println(kilometersPerHour + " km/h= " + milesPerHour + " mi/h");
+        }
+    }
+}
