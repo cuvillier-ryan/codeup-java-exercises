e45086d964a6b0d522c4bbd4cb3fbf7b809d8eb5
Ryan Cuvillier <cuvillier.ryan@gmail.com>
2020-12-10 14:47:20 -0600

    udemyJava

--- src/MyJava.java
+++ src/MyJava.java
@@ -201,7 +201,23 @@ public class MyJava {
         }
         int hours = minutes / 60;
         int remainingMinutes = minutes % 60;
-        return hours + "h " + remainingMinutes + "m " + seconds + "s";
+
+        String hoursString = hours + "h";
+        if(hours < 10){
+            hoursString = "0" + hoursString;
+        }
+
+        String minutesString = remainingMinutes + "m";
+        if(remainingMinutes < 10){
+            minutesString = "0" + minutesString;
+        }
+
+        String secondsString = seconds + "s";
+        if(seconds < 10){
+            secondsString = "0" + secondsString;
+        }
+
+        return hoursString + " " + minutesString + " " + secondsString + " ";
     }
 
     public static String getDurationString(int seconds){
