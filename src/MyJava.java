bbb319630db5797a2b45cfe2f13ba4dc403e2690
Ryan Cuvillier <cuvillier.ryan@gmail.com>
2020-12-12 11:23:05 -0600

    udemyJava

--- src/MyJava.java
+++ src/MyJava.java
@@ -248,9 +248,13 @@ public class MyJava {
 
     public static void printYearsAndDays(long minutes) {
         if (minutes < 0) {
-            System.out.println("Invalid value");
+            System.out.println("Invalid Value");
+        } else {
+            long hours = minutes / 60;
+            long days = hours / 24;
+            long years = days / 365;
+            System.out.println(minutes + " min = " + years + " y and " + days + " d");
         }
-
     }
 
 
