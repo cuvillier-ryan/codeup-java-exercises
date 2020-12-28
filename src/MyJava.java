7f759a7c1faa950498d82c36a82e89ab89b1387f
Ryan Cuvillier <cuvillier.ryan@gmail.com>
2020-12-28 08:23:33 -0600

    update

--- src/MyJava.java
+++ src/MyJava.java
@@ -261,6 +261,21 @@ public class MyJava {
         }
     }
 
+        public static int quarterOf(int month) {
+            if(month <= 0 || month > 12){
+                return -1;
+            } else if(month <= 3){
+                return 1;
+            } else if(month <= 6){
+                return 2;
+            } else if(month <= 9){
+                return 3;
+            } else{
+                return 4;
+            }
+
+        }
+
 
 //    public static double calcFeetAndInchesToCentimeters(double inches){
 //        if(inches >= 0){
